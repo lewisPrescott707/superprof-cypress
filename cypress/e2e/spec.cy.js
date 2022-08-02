cf403be8385d25c19dae4d1c78c9afc39d0d0752
prescottlewis707 <prescottlewis707@gmail.com>
2022-08-02 13:05:58 +0100

    examples

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,49 @@
-describe('empty spec', () => {
+describe('spec', () => {
+
+  before(function () {
+    cy.log("first")
+  })
+
+  beforeEach(() => {
+    cy.log("second")
+  })
+
+  afterEach(function () {
+    cy.log("penultimate")
+  })
+
+  after(() => {
+    cy.log("last")
+  })
+  
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    var url = "example"
+    cy.visit(`https://${url}.cypress.io`)
+  })
+
+  it.skip('scope', () => {
+    cy.visit(`https://${url}.cypress.io`)
+  })
+
+  it('types', () => {
+    var obj = {
+      url: "example"
+    }
+    cy.visit(`https://${obj.url}.cypress.io`)
+  })
+
+  it('async', () => {
+    ['example', 'example', 'example'].forEach((url) => {
+      cy.visit(`https://${url}.cypress.io`)
+      cy.wait(300)
+      cy.get('h1').should('have.text', 'Kitchen Sink')
+    })
+  })
+
+  it('chaining & promises', () => {
+    cy.visit(`https://example.cypress.io`).url().should('include', 'example')
+    cy.contains('get').click().then(() => {
+      cy.get('#get > a').should('have.text', 'cy.get()')
+    })
   })
 })
