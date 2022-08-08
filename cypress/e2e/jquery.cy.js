96d26bdc611a0157ac5c4d604cb0a2d3af4787b2
prescottlewis707 <prescottlewis707@gmail.com>
2022-08-08 21:34:01 +0100

    cross domain examples

--- cypress/e2e/jquery.cy.js
+++
@@ -1,8 +0,0 @@
-describe('jquery', () => {
-    it('should passes', () => {
-        cy.visit(`https://example.cypress.io`)
-        cy.get('.dropdown-toggle').click()
-        cy.contains('a', 'Querying').click()
-        cy.get('h1').should('have.text', 'Querying')
-    })
-})
