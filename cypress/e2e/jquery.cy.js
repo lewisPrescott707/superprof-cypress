140e70c2647d108b1400e1bc7c30b6c0a3d74349
prescottlewis707 <prescottlewis707@gmail.com>
2022-08-08 22:06:27 +0100

    jquer

---
+++ cypress/e2e/jquery.cy.js
@@ -0,0 +1,12 @@
+describe('jquery', () => {
+    it('selectors', () => {
+        // https://api.jquery.com/category/selectors/
+        cy.visit('https://account.bbc.com/')
+        // name
+        // id
+        // class
+        // html tag
+        // text
+        // other html attributes
+    })
+})
